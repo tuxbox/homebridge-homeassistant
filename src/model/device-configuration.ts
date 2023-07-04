e1d7cfaa8058ea79edc7e6dc23f0079a13462c6e
Iulius Gutberlet <iulius@gutberlet.eu>
2023-07-04 17:17:49 +0200

    device configuration updated

--- src/model/device-configuration.ts
+++ src/model/device-configuration.ts
@@ -2,6 +2,7 @@ import { DeviceDetails } from './device-details';
 
 export type DeviceConfiguration = {
   unique_id : string;
+  device_class: string | null;
   state_topic: string;
   value_template: string;
   name: string;
